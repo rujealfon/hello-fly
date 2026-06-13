874f7e61f029a4cd58ddbff540875cc1d47a1391
Ruje Alfon <rujealfon@users.noreply.github.com>
2026-06-13 20:58:03 +0800

    Update title in home page rendering

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Fly.io Demo Test' });
+  res.render('index', { title: 'Fly.io Demo Test 1' });
 });
 
 module.exports = router;
